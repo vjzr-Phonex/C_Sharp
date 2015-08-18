c4f5ecb8ccfa4d619fb7f490c1c74cf4f91254ae
jiangzhongran <jiangzhongran@163.com>
2015-08-18 10:20:36 +0800

    add

--- jiangzhongranLearn/ChapterSix/Program.cs
+++ jiangzhongranLearn/ChapterSix/Program.cs
@@ -9,20 +9,25 @@ namespace ChapterSix
     {
         static void Main(string[] args)
         {
-            var helloCollection = new HelloCollection();
-            byte? test = 1;
-            string s1 = "abc";
-            string s2 = "ab" + "c";
-            if(s1 == s2)
-            {
-                Console.WriteLine("s1 equals s2!");
-            }
-            byte te2 = test ?? 10;
-            if("12" is object)
-            {
-                Console.WriteLine(typeof(HelloCollection));
-            }
-            Console.WriteLine(te2);
+            Vector v1 = new Vector(1,2,3);
+            Vector v2 = new Vector(4,5,6);
+            Vector v3 = v1 + v2;
+            Console.WriteLine(v3);
+
+            //var helloCollection = new HelloCollection();
+            //byte? test = 1;
+            //string s1 = "abc";
+            //string s2 = "ab" + "c";
+            //if(s1 == s2)
+            //{
+            //    Console.WriteLine("s1 equals s2!");
+            //}
+            //byte te2 = test ?? 10;
+            //if("12" is object)
+            //{
+            //    Console.WriteLine(typeof(HelloCollection));
+            //}
+            //Console.WriteLine(te2);
 
             //var helloCollection = new HelloCollection();
             //foreach (var i in helloCollection)
---
+++ jiangzhongranLearn/ChapterSix/Vector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChapterSix
+{
+    struct Vector
+    {
+        public double x, y,z;
+
+        public Vector(double x,double y,double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector(Vector temp)
+        {
+            this.x = temp.x;
+            this.y = temp.y;
+            this.z = temp.z;
+        }
+
+        public override string ToString()
+        { 
+            return "("+x+","+y+","+z+")";
+        }
+
+        public static Vector operator + (Vector lhs,Vector rhs)
+        {
+            Vector result = new Vector(lhs);
+            result.x += rhs.x;
+            result.y += rhs.y;
+            result.z += rhs.z;
+            return result;
+        }
+    }
+}
