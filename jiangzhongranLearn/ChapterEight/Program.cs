77db6eb0f849f3065d311de85642e4334dcdfc58
jiangzhongran <jiangzhongran@163.com>
2015-08-26 20:17:16 +0800

    add

---
+++ jiangzhongranLearn/ChapterEight/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChapterEight
+{
+    class Program
+    {
+        public delegate string GetAString();
+
+        static void Main(string[] args)
+        {
+            int a = 20;
+            GetAString gas = new GetAString(a.ToString);
+            Console.WriteLine("First Delegate:"+gas());
+        }
+    }
+}
