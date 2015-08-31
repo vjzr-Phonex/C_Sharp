ec6f459579aff45876805105c1d548e50e8ef784
jiangzhongran <jiangzhongran@163.com>
2015-08-31 17:46:59 +0800

    continue add ChapterEight some files!

--- jiangzhongranLearn/ChapterEight/Program.cs
+++ jiangzhongranLearn/ChapterEight/Program.cs
@@ -35,8 +35,17 @@ namespace ChapterEight
 
             for (int i = 0; i < operations.Length;i++ )
             {
-
+                Console.WriteLine("Using operations[{0}]:",i);
+                ProcessAndDisplayNumber(operations[i],2.0);
+                ProcessAndDisplayNumber(operations[i],7.94);
+                ProcessAndDisplayNumber(operations[i],1.414);
             }
+        }
+
+        static void ProcessAndDisplayNumber(DoubleOp action, double value)
+        {
+                double result = action(value);
+                Console.WriteLine("Value is {0},result of operation is {1}",value,result);
         }
     }
 }
