cda66ca9d4d2fc93d69b9ba6b3c7c43626505d08
jiangzhongran <jiangzhongran@163.com>
2015-10-08 16:46:15 +0800

    learn pattern in chapter 9 & give a start of chapter 10.

--- jiangzhongranLearn/ChapterNine/Program.cs
+++ jiangzhongranLearn/ChapterNine/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ChapterNine
 {
@@ -9,8 +10,16 @@ namespace ChapterNine
     {
         static void Main(string[] args)
         {
-            Vector v = new Vector();
-            Console.WriteLine("The double is{0,10:E},the int is:{1}",212.223,12);
+            string str = "vala mogulis,vala dohaeris!";
+            string patString = "va";
+            MatchCollection matchCol = Regex.Matches(str,patString,RegexOptions.IgnoreCase|RegexOptions.ExplicitCapture);
+            foreach(Match nextMatch in matchCol)
+            {
+                Console.WriteLine(nextMatch.Index);
+            }
+
+            //Vector v = new Vector();
+            //Console.WriteLine("The double is{0,10:E},the int is:{1}", 212.223, 12);
         }
     }
 }
