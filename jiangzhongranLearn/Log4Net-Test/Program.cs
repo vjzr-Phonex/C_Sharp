53948c2cbf28f90d81a34aaafd8b87842a368260
jiangzhongran <jiangzhongran@163.com>
2015-10-26 17:51:02 +0800

    add web

--- jiangzhongranLearn/Log4Net-Test/Program.cs
+++ jiangzhongranLearn/Log4Net-Test/Program.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TestLog4Net;
+using System.Reflection;
+using log4net;
 
 namespace Log4Net_Test
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            LogHelper.WriteLog(typeof(Program), "ddddd");
+//            LogHelper.WriteLog(typeof(Program), "ddddd");
+//        http://blog.csdn.net/zhoufoxcn/article/details/2220533
+            ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         }
     }
 }
