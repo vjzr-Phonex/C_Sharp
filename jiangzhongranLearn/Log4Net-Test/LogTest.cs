4b5429c71c1a35bac35bec1bae56e1db5caa87f2
jiangzhongran <jiangzhongran@163.com>
2015-10-26 17:37:46 +0800

    add some files to log4net-test

---
+++ jiangzhongranLearn/Log4Net-Test/LogTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestLog4Net;
+
+namespace Log4Net_Test
+{
+    class LogTest
+    {
+        public void test() 
+        {
+            LogHelper.WriteLog(typeof(LogTest),"sdsds");
+            Exception ex = new Exception();
+            LogHelper.WriteLog(typeof(LogTest), ex);
+        }
+    }
+}
--- jiangzhongranLearn/Log4Net-Test/Program.cs
+++ jiangzhongranLearn/Log4Net-Test/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TestLog4Net;
 
 namespace Log4Net_Test
 {
@@ -9,6 +10,7 @@ namespace Log4Net_Test
     {
         static void Main(string[] args)
         {
+            LogHelper.WriteLog(typeof(Program), "ddddd");
         }
     }
 }
