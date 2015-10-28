c2b92298388e85e255ce7c04136393767594875e
jiangzhongran <jiangzhongran@163.com>
2015-10-28 10:23:07 +0800

    add some files

--- jiangzhongranLearn/Log4Net-Test/LogHelper.cs
+++ jiangzhongranLearn/Log4Net-Test/LogHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+//[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace TestLog4Net
 {
     public class LogHelper
--- jiangzhongranLearn/Log4Net-Test/Program.cs
+++ jiangzhongranLearn/Log4Net-Test/Program.cs
@@ -6,17 +6,36 @@ using TestLog4Net;
 using System.Reflection;
 using log4net;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace Log4Net_Test
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
-//            LogHelper.WriteLog(typeof(Program), "ddddd");
-//        http://blog.csdn.net/zhoufoxcn/article/details/2220533
-            ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+//            LogHelper.WriteLog(typeof(Program),"sddsew");
+
+
+//          Application.Run(new MainForm());
+            //创建日志记录组件实例
+            
+            ILog log = LogManager.GetLogger(typeof(Program));
+            //记录错误日志
+            for (int i = 0; i < 100; i++)
+            { 
+                log.Error("error", new Exception("发生了一个异常"));
+                Console.Write(i + " ");
+            }
+                //记录严重错误
+//            log.Fatal("fatal", new Exception("发生了一个致命错误"));
+            //记录一般信息
+//            log.Info("info");
+            //记录调试信息
+//            log.Debug("debug");
+            //记录警告信息
+//            log.Warn("warn");
+//            Console.WriteLine("日志记录完毕。");
+//            Coddnsole.Read();
         }
     }
 }
