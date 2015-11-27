701449afeb682d983b77299b3538f232e907cd1c
jiangzhongran <jiangzhongran@163.com>
2015-11-28 03:30:26 +0800

    add some files about pic zoom !

--- PictureZoom/PictureZoom/Form1.Designer.cs
+++ PictureZoom/PictureZoom/Form1.Designer.cs
@@ -1,12 +1,15 @@
-﻿namespace PictureZoom
+﻿using System.Windows.Forms;
+using System.Drawing;
+namespace PictureZoom
 {
-    partial class Form1
+    partial class Form1 : Form
     {
         /// <summary>
         /// 必需的设计器变量。
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+        private PictureBox pb = new PictureBox();
+        Bitmap bit = new Bitmap(300, 300);
         /// <summary>
         /// 清理所有正在使用的资源。
         /// </summary>
@@ -20,20 +23,42 @@
             base.Dispose(disposing);
         }
 
-        #region Windows 窗体设计器生成的代码
-
-        /// <summary>
-        /// 设计器支持所需的方法 - 不要
-        /// 使用代码编辑器修改此方法的内容。
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Text = "Form1";
+            this.Size = new Size(502,502);
+       
+           
+            pb.MinimumSize = new Size(500,500);
+            pb.Location = new Point(0,0);
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+            pb.Image = Image.FromFile(@"C:\Users\Phoenix\Desktop\aaa.jpg");
+            this.Controls.Add(pb);
+                       
         }
 
-        #endregion
+        public void selectPartofThePicture(PictureBox pb)
+        {
+            Graphics g = Graphics.FromImage(bit);
+            g.DrawImage(pb.Image, new Rectangle(0, 0, pb.Width, pb.Height), new Rectangle(300, 200, 100, 100), GraphicsUnit.Pixel);
+            //Image img = Image.FromHbitmap(bit.get);
+            Image img = bit;
+            //g.DrawImage(pb.Image,new Point(150,150));
+            PictureBox pb1 = new PictureBox();
+            pb1.Location = new Point(100,100);
+            pb1.Image = bit;
+            
+            pb1.Parent = pb;
+            //this.Controls.Add(pb1);
+            //pb1.Image = bit;
+            //f.Controls.Add(pb1);
+            //f.Show();
+            bit.Save(@"C:\Users\Phoenix\Desktop\v.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+            bit.Dispose();         
+        }
+
     }
 }
 
--- PictureZoom/PictureZoom/Form1.cs
+++ PictureZoom/PictureZoom/Form1.cs
@@ -14,6 +14,41 @@ namespace PictureZoom
         public Form1()
         {
             InitializeComponent();
+            this.pb.MouseClick += new MouseEventHandler(MyMouseClick);
+            this.MouseWheel += new MouseEventHandler(MyMouseWheel);
+            //this.pb.Paint +=new PaintEventHandler(pb_Paint);
         }
+
+        private void pb_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Image image = bit;
+            g.DrawImage(image, new Point(20, 10));
+        }
+
+        private void MyMouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                MessageBox.Show("滚轮向前滚动");
+            }
+            else
+            {
+                MessageBox.Show("滚轮向后滚动");
+            }
+        }
+
+        private void MyMouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            selectPartofThePicture(pb);
+            if (e.Button == MouseButtons.Right || e.Button == MouseButtons.Left)
+            {
+                Point p = MousePosition;//获取位置
+                //其他需要执行的代码
+
+            }
+
+        }
+
     }
 }
