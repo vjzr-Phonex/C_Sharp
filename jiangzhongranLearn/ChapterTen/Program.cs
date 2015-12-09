f89ca7176e8a1746d63ac1d260f276f7c7d973e4
jiangzhongran <jiangzhongran@163.com>
2015-12-09 09:32:48 +0800

    make some changes.

--- jiangzhongranLearn/ChapterTen/Program.cs
+++ jiangzhongranLearn/ChapterTen/Program.cs
@@ -7,13 +7,30 @@ namespace ChapterTen
 {
     class Program
     {
-        public static void test(int i=0) 
-        {
-            Console.WriteLine(i);
-        }
+        public static List<Racer> racers = new List<Racer>(20);
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("abc".CompareTo("abd"));
+            for (int i = 0; i < racers.Count();i++ )
+            {
+                Console.WriteLine(racers[i]);
+            }
+            Console.WriteLine();
+        }
+
+        public static void AddRacers()
+        {
+            Racer graham = new Racer(7, "Graham", "Hill", "UK", 14);
+            Racer emerson = new Racer(13, "Emerson", "Fittipaldi", "Brazil", 14);
+            Racer mario = new Racer(16, "Mario", "Andretti", "USA", 12);
+
+            racers.Add(new Racer(24, "Michael", "Schumacher", "Germany", 91));
+            racers.Add(new Racer(27, "Mika", "Hakkinen", "Finland", 20));
+
+            racers.AddRange(new Racer[]{
+                new Racer(14,"Niki","Lauda","Austria",25),
+                new Racer(27,"Alain","Porst","France",51)});
+            racers.Insert(2, new Racer(10, "lilei", "hanmeimei", "China", 2));
         }
     }
 }
