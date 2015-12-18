af38d1c770c0d7fc52f9a498abf35d61ee76c47a
jiangzhongran <jiangzhongran@163.com>
2015-12-18 09:01:53 +0800

    add some files in chapter ten.

---
+++ jiangzhongranLearn/ChapterTen/EmployeeId.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChapterTen
+{
+    [Serializable]
+    public class EmployeeId : IEquatable<EmployeeId>
+    {
+        private readonly char prefix;
+        private readonly int number;
+
+        public EmployeeId()
+        { 
+            
+        }
+    }
+}
