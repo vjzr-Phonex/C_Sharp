53523644dd0ac04813a93bdca798b0bc349e76c0
jiangzhongran <jiangzhongran@163.com>
2015-12-18 14:07:33 +0800

    add some code in chapter ten

--- jiangzhongranLearn/ChapterTen/EmployeeId.cs
+++ jiangzhongranLearn/ChapterTen/EmployeeId.cs
@@ -11,9 +11,46 @@ namespace ChapterTen
         private readonly char prefix;
         private readonly int number;
 
-        public EmployeeId()
-        { 
-            
+        public EmployeeId(string id)
+        {
+            if (id == null)
+                throw new ArgumentException("ID is wrong!");
+            this.prefix = (id.ToUpper())[0];
+            int numLength = id.Length;
+
+            try
+            {
+                number = int.Parse(id.Substring(1, numLength > 6 ? 6 : numLength));
+            }
+            catch (FormatException)
+            {
+                throw new EmployeeIdException("Invalid EmployeeId format!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return prefix.ToString() + string.Format("{0,6:000000}", number);
+        }
+
+        public override int GetHashCode()
+        {
+            return (number ^ number)*0x15051505;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals((EmployeeId)obj);
+        }
+
+        public static bool operator ==(EmployeeId left, EmployeeId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(EmployeeId left, EmployeeId right)
+        {
+            return !(left == right);
         }
     }
 }
