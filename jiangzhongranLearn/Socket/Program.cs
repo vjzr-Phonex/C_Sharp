a2b7537900a2d9221657ffcba3855d1656db549c
jiangzhongran <jiangzhongran@163.com>
2016-02-15 14:34:00 +0800

    add some code in C# Socket

---
+++ jiangzhongranLearn/Socket/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server("127.0.0.1", 2000);
+            server.startServer();
+        }
+    }
+}
---
+++ jiangzhongranLearn/SocketClient/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace SocketClient
+{
+    class Client
+    {
+        private string host;
+        private int port;
+
+        public Client(string host,int port)
+        {
+            this.init(host,port);
+        }
+
+        public void init(string host, int port) 
+        {
+            this.host = host;
+            this.port = port;
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip,port);
+
+        }
+    }
+}
