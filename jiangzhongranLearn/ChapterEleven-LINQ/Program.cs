559552198fe5480a15479068f391d4d2a9405b24
jiangzhongran <jiangzhongran@163.com>
2016-06-16 16:50:49 +0800

    add some class.

--- jiangzhongranLearn/ChapterEleven-LINQ/Program.cs
+++ jiangzhongranLearn/ChapterEleven-LINQ/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace ChapterEleven_LINQ
 {
@@ -9,6 +10,40 @@ namespace ChapterEleven_LINQ
     {
         static void Main(string[] args)
         {
+            ttt();
+        }
+
+        static void ttt()
+        {
+            long time1 = Environment.TickCount;
+            long time2 = Environment.TickCount;
+            long tmp = time2 - 0;
+        }
+
+
+        private static void test()
+        {
+            try
+            {
+                test2();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("catch ex in floor2!");
+            }
+        }
+
+        private static void test2 ()
+        {
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("catch ex in floor 1!");
+                throw new Exception();
+            }
         }
     }
 }
