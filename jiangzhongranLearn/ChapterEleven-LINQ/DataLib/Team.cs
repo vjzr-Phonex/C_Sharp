fff4e4c59b7d4a4216cf9bdf545e0101579e241e
jiangzhongran <jiangzhongran@163.com>
2016-06-20 17:36:00 +0800

    add some classes.

--- jiangzhongranLearn/ChapterEleven-LINQ/DataLib/Team.cs
+++ jiangzhongranLearn/ChapterEleven-LINQ/DataLib/Team.cs
@@ -7,9 +7,12 @@ namespace ChapterEleven_LINQ
 {
     class Team
     {
+        public string Name { set; get; }
+        public int[] Years { set; get; }
         public Team(string name, params int[] years)
-        { 
-            
+        {
+            this.Name = name;
+            this.Years = years;
         }
     }
 }
